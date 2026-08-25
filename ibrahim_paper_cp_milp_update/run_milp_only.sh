d81571aca505f0f142cf477c6acba8e20f532750
alimohammadi44 <ali_mohammadi@yahoo.com>
2026-08-24 22:28:39 -0400

    Clean QIML UAV HUBO benchmark package

--- ibrahim_paper_cp_milp_update/run_milp_only.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")"/src
-python cp_milp_exact_baselines.py --mode milp --time-limit 300 "$@"
--- ibrahim_paper_cp_milp_update/run_quav_style_baseline.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")"/src
-python quav_style_baseline.py "$@"
